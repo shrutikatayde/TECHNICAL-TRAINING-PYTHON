753158e2548271c752117f46c4fc49b53e378b3a
SHRUTIKA TAYDE <89908129+shrutikatayde@users.noreply.github.com>
2023-09-13 23:30:35 +0530

    Day 1

--- 2_zombie.py
+++ 2_zombie.py
@@ -4,21 +4,25 @@ Created on Wed Aug 23 15:24:16 2023
 
 @author: Lenovo
 """
+
+
 def Zombie(bobEnergy, n_OfZombie, arr_energyOfZombie):
-    for i in range(0, len(arr_energyOfZombie)-1):
-        bobEnergy-=(arr_energyOfZombie[i]%2)+(arr_energyOfZombie[i]/2)
-        if bobEnergy<=arr_energyOfZombie[i]:
-            res="No"
-        elif bobEnergy>arr_energyOfZombie[i]:
-            res="Yes"
-        ''' elif bobEnergy==-1:
-            return'''
-        
+    res = ""
+    for i in range(0, n_OfZombie - 1):
+        bobEnergy -= (arr_energyOfZombie[i] % 2) + (arr_energyOfZombie[i] / 2)
+        if bobEnergy <= arr_energyOfZombie[i]:
+            res = "No"
+        elif bobEnergy > arr_energyOfZombie[i]:
+            res = "Yes"
+        """ elif bobEnergy==-1:
+            return"""
+
     return res
 
-bobEnergy, n_OfZombie=list(map(int,input("Enter Bob Energy  and Zombies Count : ").split()))
-arr_energyOfZombie=list(map(int,input("Enter Energy of each zombie : ").split()))
-res=Zombie(bobEnergy, n_OfZombie, arr_energyOfZombie)
-print(res)
-                           
-            
+
+bobEnergy, n_OfZombie = list(
+    map(int, input("Enter Bob Energy  and Zombies Count : ").split())
+)
+arr_energyOfZombie = list(map(int, input("Enter Energy of each zombie : ").split()))
+result = Zombie(bobEnergy, n_OfZombie, arr_energyOfZombie)
+print(result)
